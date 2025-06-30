b4507996c64d8f37a76ea8d042cd0d4405b101f1
kirych95 <kirych2@yandex.ru>
2025-06-30 17:26:57 +0300

    практика онлайн

--- OnlinePractice/OnlinePractice5.2/js/script.js
+++ OnlinePractice/OnlinePractice5.2/js/script.js
@@ -5,3 +5,8 @@ aBtn.addEventListener('click', () => {
     aBody.classList.toggle("accordeon-body-open") //toggle - это проверка, если есть удаляет, если нет добавляет
     aBtn.classList.toggle("accordeon-btn-open")
 })
+
+const changeTheme = document.querySelector("#change-theme")
+changeTheme.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme")
+})
