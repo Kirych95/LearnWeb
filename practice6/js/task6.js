27e8309bd0d2a792885d203930326bcb75a6d843
Kirych95 <kirych2@yandex.ru>
2025-07-06 00:42:49 +0300

    Финал 6 задания

--- practice6/js/task6.js
+++ practice6/js/task6.js
@@ -1,26 +1,10 @@
-const mainPicture = document.querySelector("#main-picture")
-const picture1 = document.querySelector("#picture-1")
-const picture2 = document.querySelector("#picture-2")
-const picture3 = document.querySelector("#picture-3")
-const picture4 = document.querySelector("#picture-4")
-const picture5 = document.querySelector("#picture-5")
+document.addEventListener('DOMContentLoaded', function () {
+    const mainPicture = document.querySelector('#main-picture')
+    const pictures = document.querySelectorAll('.gallery-pictures img')
 
-picture1.addEventListener("click", () => {
-    mainPicture.src = picture1.src
-})
-
-picture2.addEventListener("click", () => {
-    mainPicture.src = picture2.src
-})
-
-picture3.addEventListener("click", () => {
-    mainPicture.src = picture3.src
-})
-
-picture4.addEventListener("click", () => {
-    mainPicture.src = picture4.src
-})
-
-picture5.addEventListener("click", () => {
-    mainPicture.src = picture5.src
+    pictures.forEach(picture => {
+        picture.addEventListener('click', function() {
+            mainPicture.src = this.src
+        })
+    })
 })
