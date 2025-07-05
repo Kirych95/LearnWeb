2b87f9aeca92cc9fce89002fe90c50da22673f9b
Kirych95 <kirych2@yandex.ru>
2025-07-05 21:55:46 +0300

    то что не догнал

---
+++ OnlinePractice/OnlinePractice5.4/js/script.js
@@ -0,0 +1,45 @@
+const URL = "https://odinkeane.github.io/web-developer/data.json"
+const cardsDiv = document.querySelector("#cards")
+let result
+let basket = JSON.parse(localStorage.getItem("basket")) || []
+
+
+
+window.addEventListener("load", async () => {
+    result = await fetch(URL)
+        .then(res => res.json())
+    generateCards()
+})
+
+
+function generateCards(){
+    for (let card of result){
+        cardsDiv.innerHTML += generateCardHTML(card)
+    }
+}
+
+function generateCardHTML(card){
+    return `
+        <div class='card'>
+            <img src=${card.imageURL}>
+            <h3>${card.name}</h3>
+            <p>${card.price*79}</p>
+            <button onclick='addToBasket(${card.id})'>
+                Добавить
+            </button>
+        </div>
+    `
+}
+function addToBasket(id){
+    for (let card of result){
+        if (card.id == id){
+            basket.push(card)
+            break
+        }
+    }
+    localStorage.setItem("basket", JSON.stringify(basket))
+    
+}
+
+
+
