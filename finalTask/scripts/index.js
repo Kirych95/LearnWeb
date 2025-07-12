f840c7373afb986c8dc92652ebba51c153e9a8ae
Kirych95 <kirych2@yandex.ru>
2025-07-13 01:44:02 +0300

    header

--- finalTask/scripts/index.js
+++ finalTask/scripts/index.js
@@ -1,3 +1,4 @@
+/* Бургер-меню */
 const burgerMenuBtn = document.querySelector("#burger-menu-btn")
 const burgerMenu = document.querySelector("#burger-menu")
 const burgerMenuClose = document.querySelector("#burger-menu-close")
@@ -8,4 +9,32 @@ burgerMenuBtn.addEventListener("click", () => {
 
 burgerMenuClose.addEventListener("click", () => {
     burgerMenu.classList.remove("burger-menu-open")
+})
+
+/* Переход по ссылкам навигации */
+const navLinks = document.querySelectorAll("nav a")
+navLinks[0].addEventListener("click", (e) => {
+    e.preventDefault()
+    const element = document.querySelector("#hero")
+    element.scrollIntoView({ behavior: "smooth" })
+})
+navLinks[1].addEventListener("click", (e) => {
+    e.preventDefault()
+    const element = document.querySelector("#about")
+    element.scrollIntoView({ behavior: "smooth" })
+})
+navLinks[2].addEventListener("click", (e) => {
+    e.preventDefault()
+    const element = document.querySelector("#menu")
+    element.scrollIntoView({ behavior: "smooth" })
+})
+navLinks[3].addEventListener("click", (e) => {
+    e.preventDefault()
+    const element = document.querySelector("#reviews")
+    element.scrollIntoView({ behavior: "smooth" })
+})
+navLinks[4].addEventListener("click", (e) => {
+    e.preventDefault()
+    const element = document.querySelector("#contacts")
+    element.scrollIntoView({ behavior: "smooth" })
 })
