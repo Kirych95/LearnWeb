e9e232e1c8217497ab560f5696acaabe5a7f0854
Kirych95 <kirych2@yandex.ru>
2025-07-13 20:24:48 +0300

    finish advantages + adaptive

--- finalTask/scripts/index.js
+++ finalTask/scripts/index.js
@@ -20,7 +20,7 @@ navLinks[0].addEventListener("click", (e) => {
 })
 navLinks[1].addEventListener("click", (e) => {
     e.preventDefault()
-    const element = document.querySelector("#about")
+    const element = document.querySelector("#advantages")
     element.scrollIntoView({ behavior: "smooth" })
 })
 navLinks[2].addEventListener("click", (e) => {
@@ -48,7 +48,7 @@ navLinks[5].addEventListener("click", (e) => {
 })
 navLinks[6].addEventListener("click", (e) => {
     e.preventDefault()
-    const element = document.querySelector("#about")
+    const element = document.querySelector("#advantages")
     element.scrollIntoView({ behavior: "smooth" })
     burgerMenu.classList.remove("burger-menu-open")
 })
@@ -82,4 +82,18 @@ heroButtons[1].addEventListener("click", (e) => {
     e.preventDefault()
     const element = document.querySelector("#contacts")
     element.scrollIntoView({ behavior: "smooth" })
+})
+
+/* Слайдер преимуществ */
+const elements = document.querySelectorAll(".element")
+const buttons = document.querySelectorAll(".buttons button")
+
+buttons.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+        buttons.forEach((btn) => btn.style.borderBottom = "")
+        btn.style.borderBottom = "2px solid var(--primary-color)"
+        for (let el of elements) {
+            el.style.left = -index * 120 + "%"
+        }
+    })
 })
